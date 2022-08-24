8771592151ca5d9ec2ac9fcdd1f349a187b3e2dd
Nagarjun <89917201+jeyanagarjun@users.noreply.github.com>
2022-08-24 11:35:06 +0530

    Add files via upload

---
+++ scripted.groovy
@@ -0,0 +1,12 @@
+def BuildApp() {
+	echo "its is builded successfully"
+	}
+def TestApp()  {
+	echo "its is tested successfully"
+	}
+def DeployApp()  {
+	echo "its is deployed successfully"
+	}
+	
+return this		
+	
